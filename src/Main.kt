b3c713bde33d486f334daa459bcb30f4bef606a6
ilhom <fozilov.7459045@gmail.com>
2024-02-10 16:23:59 +0500

    show object in actions

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,35 @@
-
 fun main() {
+    val sampleObject = Sample.newInstance()
+    sampleObject.counder++
+    val sampleObject2 = Sample.newInstance()
+    sampleObject2.counder++
+    println(sampleObject2.counder)
+    println(sampleObject.counder)
     print("Hello wotld!")
+}
+
+class Sample private constructor() {
+
+    var counder = 0
+
+    companion object {
+
+        private var sample: Sample? = null
+
+        fun newInstance(): Sample {
+            return sample ?: Sample().apply {
+                sample = this
+            }
+        }
+    }
+
+}
+
+object Sample2 {
+
+    var counder = 0
+}
+
+fun String.name() {
+
 }
