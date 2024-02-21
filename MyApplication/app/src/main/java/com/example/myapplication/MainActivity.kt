450ca2cdc6863fa701af629f911f6e920ca52d6f
ilhom <fozilov.7459045@gmail.com>
2024-02-21 19:15:23 +0500

    2 урок верстка окон

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.progressindicator.LinearProgressIndicator
@@ -12,15 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val linearProgressIndicator = findViewById<LinearProgressIndicator>(R.id.linearProgressIndicator)
-        val progressTextView = findViewById<TextView>(R.id.progressTextView)
-        val button = findViewById<Button>(R.id.button)
-        button.setOnClickListener {
-            val currentProgress = linearProgressIndicator.progress + 2
-            progressTextView.text = currentProgress.toString()
-            linearProgressIndicator.setProgress(currentProgress,true)
-        }
+//        val linearProgressIndicator = findViewById<LinearProgressIndicator>(R.id.linearProgressIndicator)
+//        val progressTextView = findViewById<TextView>(R.id.progressTextView)
+//        val button = findViewById<Button>(R.id.button)
+//        button.setOnClickListener {
+//            val currentProgress = linearProgressIndicator.progress + 2
+//            progressTextView.text = currentProgress.toString()
+//            linearProgressIndicator.setProgress(currentProgress,true)
+//        }
     }
 
 }
