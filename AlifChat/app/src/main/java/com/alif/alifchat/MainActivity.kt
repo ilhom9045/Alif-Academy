47747cf53d9efa9840dbc6b3bffe2d96afb2714f
Zafar <zafarbobojonov05032005@gmail.com>
2024-03-14 13:58:32 +0500

    no toolbar

--- AlifChat/app/src/main/java/com/alif/alifchat/MainActivity.kt
+++ AlifChat/app/src/main/java/com/alif/alifchat/MainActivity.kt
@@ -2,9 +2,13 @@ package com.alif.alifchat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.alif.alifchat.messenger.fragment.presentation.ToolBarFragment
 import com.alif.alifchat.messenger.fragment.presentation.fragment.ChatDetailFragment
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
